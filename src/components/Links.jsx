b22745d57af4748f4309f37fd66e8423602eef22
Coleman <c.bouch@live.ca>
2021-08-09 17:08:10 -0500

    First real commit

---
+++ src/components/Links.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Collapse = styled.div.attrs({
+    className: 'collpase navbar-collapse',
+})``
+
+const List = styled.div.attrs({
+    className: 'navbar-nav mr-auto',
+})``
+
+const Item = styled.div.attrs({
+    className: 'collpase navbar-collapse',
+})``
+
+class Links extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Link to="/" className="navbar-brand">
+                    VDW - Inventory Management System
+                </Link>
+                <Collapse>
+                    <List>
+                        <Item>
+                            <Link to="/movies/list" className="nav-link">
+                                List View
+                            </Link>
+                        </Item>
+                        <Item>
+                            <Link to="/movies/create" className="nav-link">
+                                Test Entry
+                            </Link>
+                        </Item>
+                    </List>
+                </Collapse>
+            </React.Fragment>
+        )
+    }
+}
+export default Links
